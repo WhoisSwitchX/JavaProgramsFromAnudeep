854c8bf6e8d81ed9f4009075215523867c3c3e39
WhoisSwitchX <105196153+WhoisSwitchX@users.noreply.github.com>
2022-06-17 11:36:00 +0530

    Add files via upload

---
+++ Lab Activity/Saurabhif.java
@@ -0,0 +1,12 @@
+public class ifstatement {
+
+	public static void main(String[] args) {
+		int n = 10;
+		if (n % 2 == 0) {
+			System.out.println("The number is even : "+ n);
+		 } else {
+			System.out.println("The number is odd : "+ n);
+		 }
+	}
+
+}
