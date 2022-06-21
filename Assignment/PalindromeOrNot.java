442fdcf3d9247dcaacda17927ef2b3eaab2eee53
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2022-06-21 21:19:57 +0530

    Add files via upload

---
+++ Assignment/PalindromeOrNot.java
@@ -0,0 +1,28 @@
+/*
+ * @ Author Saurabh
+ */
+
+package SwitchX;
+
+public class PalindromeOrNot {
+
+	public static void main(String[] args) {
+
+		int r,sum=0,temp;    
+		int n=454; 
+		temp=n;    
+		
+		while(n>0)
+		{    
+			r=n%10;   
+			sum=(sum*10)+r;    
+			n=n/10;    
+		}    
+		if(temp==sum)    
+			System.out.println("The Number is palindrome number. ");    
+		else    
+			System.out.println("The Number is not palindrome");    
+
+	}
+
+}
