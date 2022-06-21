f0ba674c98c1a33c6da7f3b454697d9b8f96ccb7
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2022-06-21 14:27:29 +0530

    Update FibonacciSeries.java

--- Assignment/FibonacciSeries.java
+++ Assignment/FibonacciSeries.java
@@ -10,7 +10,7 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		
 		int n =10, n1 = 0, n2 = 1;
-		System.out.print("Fibonacci Series of "+ n +" numbers:");
+		System.out.print("Fibonacci Series of "+ n +" numbers: ");
         for (int i = 1; i <= 10; ++i)
         {
             System.out.print(n1+"  ");
