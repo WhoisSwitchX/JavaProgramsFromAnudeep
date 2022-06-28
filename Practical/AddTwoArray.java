2871aed8a787d196118ceca151d512a20808f4b6
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2022-06-28 13:57:08 +0530

    Add files via upload

---
+++ Practical/AddTwoArray.java
@@ -0,0 +1,20 @@
+/*
+ *@ Author Saurabh Mathe 
+ */
+package SwitchX;
+
+public class AddTwoArray {
+
+	public static void main(String[] args) {
+
+		int a[]= {1,2,3,4,5};
+		int b[]= {9,8,7,6,5};
+		int c[]= new int[a.length];
+		System.out.print("Addition of Two Array(a+b): ");
+		for(int i=0; i<a.length; i++) {
+			c[i]=a[i]+b[i];
+			System.out.print(c[i]+"  ");
+		}
+	}
+
+}
---
+++ Practical/SubjectMarks.java
@@ -0,0 +1,15 @@
+package SwitchX;
+
+public class SubjectMarks {
+
+	public static void main(String[] args) {
+
+		int a [] = {90,80,70,60,50};
+		int avg=0;
+		for(int i =0; i<5;i++) {
+				 avg=avg + i;
+		}
+		System.out.println(avg);
+	}
+
+}
