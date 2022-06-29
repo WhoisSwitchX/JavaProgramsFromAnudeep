827e47048ac535afce5ec9566c829c7cd264be1a
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2022-06-29 21:56:54 +0530

    Update UserInputForTwoMatrix.java

--- Practical/UserInputForTwoMatrix.java
+++ Practical/UserInputForTwoMatrix.java
@@ -9,10 +9,10 @@ public class UserInputForTwoMatrix {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter number of rows to print matrix :");
+		System.out.print("Enter number of rows to print Matrix :");
 		int rows = sc.nextInt();
 
-		System.out.print("Enter number of coloums to print matrix :");
+		System.out.print("Enter number of coloums to print Matrix :");
 		int col = sc.nextInt();
 
 		int arr[][] = new int[rows][col];
@@ -25,9 +25,9 @@ public class UserInputForTwoMatrix {
 			}
 		}
 
-		System.out.print("Enter number of rows for 2nd matrix : ");
+		System.out.print("Enter number of rows for 2nd Matrix : ");
 		int rows2 = sc.nextInt();
-		System.out.print("Enter number of collums for 2nd matrix : ");
+		System.out.print("Enter number of collums for 2nd Matrix : ");
 		int col2 = sc.nextInt();
 
 		int arr2[][] = new int[rows2][col2];
@@ -35,7 +35,7 @@ public class UserInputForTwoMatrix {
 		{
 			for(int j = 0 ; j < arr2[i].length ; j++) 
 			{
-				System.out.print("enter value for "+(i + 1)+ "st row and "+ (j+1) + "st coloumn  : ");
+				System.out.print("Enter value for "+(i + 1)+ "st Row and "+ (j+1) + "st Coloumn  : ");
 				arr2[i][j] = sc.nextInt();
 			}
 		}
