ea0b4cb3f6b97a13d787533454c5f9e5baa7d16d
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2022-07-07 02:26:07 +0530

    Add files via upload

---
+++ Practical/TaxCalculate.java
@@ -0,0 +1,23 @@
+package SwitchX;
+import java.util.Scanner;
+class TaxCalculator{
+	float basicSalary;
+	boolean citizenShip;
+	
+	public float calculateTax () {
+		float tax = 30*basicSalary/100;
+		return tax;	
+	} 
+}
+public class TaxCalculate {
+	public static void main(String []args) {
+		Scanner ab = new Scanner(System.in);
+		TaxCalculator tc = new TaxCalculator();
+		System.out.println("Enter the salary to calculate tax: ");
+		tc.basicSalary = ab.nextFloat();
+		
+		System.out.println("The Tax of the employee for the  " + tc.basicSalary + " is "+ tc.calculateTax());
+		
+	}	
+
+}
