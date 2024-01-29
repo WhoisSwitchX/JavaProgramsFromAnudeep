180e40c03f8a1e58f5eb7bde8e2067617955ac1d
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2024-01-29 23:41:19 +0530

    Update OddNumber.java

--- Assignment/OddNumber.java
+++ Assignment/OddNumber.java
@@ -5,7 +5,7 @@ package Assignment;
 
 public class OddNumber {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		
 		for(int i=1; i<=200; i=i+2) {
 			System.out.println(i);
